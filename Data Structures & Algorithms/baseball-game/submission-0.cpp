8e2d29dd13d71b633bcf7eed93f4106713a85e06
Subodh Mishra <subodheenitr@gmail.com>
2026-07-31 09:12:03 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        std::vector<int> record;
+        for(int i = 0 ; i < operations.size(); ++i) {
+            size_t record_size = record.size();
+            if (operations[i] == "+") {
+                record.push_back(record[record_size-1]+record[record_size-2]);
+            } else if (operations[i] == "C") {
+                record.pop_back();
+            } else if (operations[i] == "D") {
+                record.push_back(2*record[record_size-1]);
+            } else {
+                record.push_back(std::stoi(operations[i]));
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < record.size(); i++){
+            sum += record[i];
+        }
+        return sum;
+    }
+};
