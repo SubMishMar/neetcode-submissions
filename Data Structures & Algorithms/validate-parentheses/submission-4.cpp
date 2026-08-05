cb100f79e78db54953487088246d6b30d462ac51
Subodh Mishra <subodheenitr@gmail.com>
2026-08-05 22:54:52 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        // Method 1
+        if(s.size()==0) {
+            return true;
+        }
+        std::vector<char> stk;
+        std::unordered_map<char, char> dict;
+        dict['}'] = '{';
+        dict[')'] = '(';
+        dict[']'] = '[';
+        for(char c : s) {
+            if(c=='{'||c=='['||c=='(') {
+                stk.push_back(c);
+            } else {
+                // has to be a closing bracket.
+                if(stk.size()==0){
+                    return false;
+                }
+                char top = stk.back();
+                if(top==dict[c]) {
+                    stk.pop_back();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(stk.size()==0) {
+            return true;
+        }
+        return false;
+    }
+};
