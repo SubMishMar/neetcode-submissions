2fffe306d3312f80fd51e26aaac90a43bfaa307a
Subodh Mishra <subodheenitr@gmail.com>
2026-08-09 00:28:55 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        // Method 1
+        // std::map<int, int> dict;
+        // for (const auto num : nums) {
+        //     dict[num]++;
+        // }
+
+        // bool hasduplicate = false;
+        // for (const auto& [key, value] : dict) {
+        //     if (value > 1) {
+        //         hasduplicate = true;
+        //         break; 
+        //     }
+        // }
+
+        // return hasduplicate;
+
+        // Method 2
+        bool has_duplicate = false;
+        std::unordered_set<int> seen;
+        for (const auto num : nums) {
+            if(seen.count(num)) {
+                has_duplicate = true;
+                break;
+            }
+            seen.insert(num);
+        }
+        return has_duplicate;
+    }
+};
