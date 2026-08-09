f326531841682f5031ca030432d3882b8c2ee515
Subodh Mishra <subodheenitr@gmail.com>
2026-08-09 10:29:31 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::map<int, int> dict;
+        for(int i = 0; i < nums.size(); i++) {
+            dict[nums[i]] = i;
+        }
+        std::vector<int> ids;
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if(dict.find(complement)!=dict.end() && i != dict[complement]) {
+                ids.push_back(i);
+                ids.push_back(dict[complement]);
+                break;
+            }
+        }
+        return ids;
+    }
+};
